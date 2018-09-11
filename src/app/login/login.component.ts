e08a8f73f321a5899eca4d0adc4405fd1c929754
Kevin Herrarte <kevinfertech@gmail.com>
2018-09-11 15:58:30 -0600

    Initialize login forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication/authentication.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,24 @@ import { AuthenticationService } from '../services/authentication/authentication
 })
 export class LoginComponent implements OnInit {
   private user: any;
+  private loginForm: FormGroup;
 
-  constructor(private service: AuthenticationService) { }
+  constructor(
+    private service: AuthenticationService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
     this.service.login().subscribe(user => this.user = user);
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return true;
+    }
   }
 
 }
