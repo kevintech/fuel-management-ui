b16cc706c7c825aeb677a676926f698d2d4b068d
Kevin Herrarte <kevinfertech@gmail.com>
2018-10-16 19:00:47 -0600

    WIP: Add fuel reporting component

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core'
-import { RouterModule, Routes } from '@angular/router'
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 /**
  * Components
  */
-import { LoginComponent } from '../components/login/login.component'
-import { HomeComponent } from '../components/home/home.component'
-import { DriversComponent } from '../components/drivers/drivers.component'
-import { DriversNewComponent } from '../components/drivers-new/drivers-new.component'
-import { DriversEditComponent } from '../components/drivers-edit/drivers-edit.component'
-import { DriverBatchLoadComponent } from '../components/driver-batch-load/driver-batch-load.component'
-import { EquipmentsComponent } from '../components/equipments/equipments.component'
-import { EquipmentBatchLoadComponent } from '../components/equipment-batch-load/equipment-batch-load.component'
-import { EquipmentNewComponent } from '../components/equipment-new/equipment-new.component'
-import { EquipmentEditComponent } from '../components/equipment-edit/equipment-edit.component'
-import { SupplyStationsComponent } from '../components/supply-stations/supply-stations.component'
-import { SupplyStationBatchLoadComponent } from '../components/supply-station-batch-load/supply-station-batch-load.component'
-import { SupplyStationsNewComponent } from '../components/supply-stations-new/supply-stations-new.component'
-import { SupplyStationsEditComponent } from '../components/supply-stations-edit/supply-stations-edit.component'
-import { FuelEntryComponent } from '../components/fuel-entry/fuel-entry.component'
-import { FuelEntryNewComponent } from '../components/fuel-entry-new/fuel-entry-new.component'
-import { FuelEntryEditComponent } from '../components/fuel-entry-edit/fuel-entry-edit.component'
+import { LoginComponent } from '../components/login/login.component';
+import { HomeComponent } from '../components/home/home.component';
+import { DriversComponent } from '../components/drivers/drivers.component';
+import { DriversNewComponent } from '../components/drivers-new/drivers-new.component';
+import { DriversEditComponent } from '../components/drivers-edit/drivers-edit.component';
+import { DriverBatchLoadComponent } from '../components/driver-batch-load/driver-batch-load.component';
+import { EquipmentsComponent } from '../components/equipments/equipments.component';
+import { EquipmentBatchLoadComponent } from '../components/equipment-batch-load/equipment-batch-load.component';
+import { EquipmentNewComponent } from '../components/equipment-new/equipment-new.component';
+import { EquipmentEditComponent } from '../components/equipment-edit/equipment-edit.component';
+import { SupplyStationsComponent } from '../components/supply-stations/supply-stations.component';
+import { SupplyStationBatchLoadComponent } from '../components/supply-station-batch-load/supply-station-batch-load.component';
+import { SupplyStationsNewComponent } from '../components/supply-stations-new/supply-stations-new.component';
+import { SupplyStationsEditComponent } from '../components/supply-stations-edit/supply-stations-edit.component';
+import { FuelEntryComponent } from '../components/fuel-entry/fuel-entry.component';
+import { FuelEntryNewComponent } from '../components/fuel-entry-new/fuel-entry-new.component';
+import { FuelEntryEditComponent } from '../components/fuel-entry-edit/fuel-entry-edit.component';
 import { OilEntryComponent } from '../components/oil-entry/oil-entry.component';
 import { OilEntryNewComponent } from '../components/oil-entry-new/oil-entry-new.component';
 import { OilEntryEditComponent } from '../components/oil-entry-edit/oil-entry-edit.component';
+import { FuelReportComponent } from '../components/fuel-report/fuel-report.component';
 
 /**
  * Guards
  */
-import { AuthGuard } from '../guards/auth.guard'
+import { AuthGuard } from '../guards/auth.guard';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -51,8 +52,9 @@ const appRoutes: Routes = [
   { path: 'entries/oil', component: OilEntryComponent, canActivate: [AuthGuard] },
   { path: 'entries/oil/new', component: OilEntryNewComponent, canActivate: [AuthGuard] },
   { path: 'entries/oil/:id', component: OilEntryEditComponent, canActivate: [AuthGuard] },
+  { path: 'reports/fuel', component: FuelReportComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'home' }
-]
+];
 
 @NgModule({
   imports: [
